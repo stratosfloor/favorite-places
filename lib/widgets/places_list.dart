158dc2f43a32b3c1737fe36444e14094501d0431
Emil Noren <stratosfloor@hotmail.com>
2023-08-29 17:49:07 +0200

    Add adress on subtitle on places list

--- lib/widgets/places_list.dart
+++ lib/widgets/places_list.dart
@@ -34,6 +34,12 @@ class PlacesList extends StatelessWidget {
                   color: Theme.of(context).colorScheme.onBackground,
                 ),
           ),
+          subtitle: Text(
+            places[index].location.adress.toString(),
+            style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
           onTap: () {
             Navigator.of(context).push(
               MaterialPageRoute(
