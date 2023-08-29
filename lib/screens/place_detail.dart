9cf5e0980df9551b98797102c140f5316d5c50bd
Emil Noren <stratosfloor@hotmail.com>
2023-08-29 12:29:59 +0200

    Image show fullscreen on place detail screen

--- lib/screens/place_detail.dart
+++ lib/screens/place_detail.dart
@@ -12,9 +12,18 @@ class PlaceDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(place.title),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(place.title),
+        ),
+        body: Stack(
+          children: [
+            Image.file(
+              place.image,
+              fit: BoxFit.cover,
+              height: double.infinity,
+              width: double.infinity,
+            ),
+          ],
+        ));
   }
 }
