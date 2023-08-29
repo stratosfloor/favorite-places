b5fc53dae39ac90a50e45f51880124d7308e0a9c
Emil Noren <stratosfloor@hotmail.com>
2023-08-29 18:09:16 +0200

    Add map preview on place detail screen

--- lib/screens/place_detail.dart
+++ lib/screens/place_detail.dart
@@ -1,4 +1,5 @@
 import 'package:favorite_places/models/place.dart';
+import 'package:favorite_places/services/google_maps_api.dart';
 import 'package:flutter/material.dart';
 
 class PlaceDetailScreen extends StatelessWidget {
@@ -23,6 +24,48 @@ class PlaceDetailScreen extends StatelessWidget {
               height: double.infinity,
               width: double.infinity,
             ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Column(
+                children: [
+                  CircleAvatar(
+                    radius: 70,
+                    backgroundImage: NetworkImage(
+                      getLocationImage(
+                        place.location.latitude,
+                        place.location.longitude,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 24,
+                      vertical: 16,
+                    ),
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          Colors.transparent,
+                          Colors.black54,
+                        ],
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                      ),
+                    ),
+                    child: Text(
+                      place.location.adress,
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                            color: Theme.of(context).colorScheme.onBackground,
+                          ),
+                    ),
+                  )
+                ],
+              ),
+            )
           ],
         ));
   }
