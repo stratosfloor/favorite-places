c5b3f1a9d537648f97ff05b194978acd2ec9214c
Emil Noren <stratosfloor@hotmail.com>
2023-08-29 17:04:05 +0200

    Add model for PlaceLocation and add that model to Place-model

--- lib/models/place.dart
+++ lib/models/place.dart
@@ -4,13 +4,26 @@ import 'package:uuid/uuid.dart';
 
 var uuid = const Uuid();
 
+class PlaceLocation {
+  const PlaceLocation({
+    required this.latitude,
+    required this.longitude,
+    required this.adress,
+  });
+  final double latitude;
+  final double longitude;
+  final String adress;
+}
+
 class Place {
   Place({
     required this.title,
     required this.image,
+    required this.location,
   }) : id = uuid.v4();
 
   final String id;
   final String title;
   final File image;
+  final PlaceLocation location;
 }
