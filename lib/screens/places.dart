df5483a53a0ee7bcc27f1463ce61a62433b36b89
Emil Noren <stratosfloor@hotmail.com>
2023-08-29 09:24:31 +0200

    Rename to places screen

--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -1,15 +1,20 @@
 import 'package:favorite_places/models/place.dart';
 import 'package:flutter/material.dart';
 
-class Places extends StatefulWidget {
-  const Places({super.key});
+class PlacesScreen extends StatefulWidget {
+  const PlacesScreen({super.key});
 
   @override
-  State<Places> createState() => _PlacesState();
+  State<PlacesScreen> createState() => _PlacesScreenState();
 }
 
-class _PlacesState extends State<Places> {
+class _PlacesScreenState extends State<PlacesScreen> {
   final List<Place> _places = [];
+
+
+  void _addPlace () {
+    Navigator.of(context).push(MaterialPageRoute(builder: (ctx) => ))
+  }
 
   @override
   Widget build(BuildContext context) {
