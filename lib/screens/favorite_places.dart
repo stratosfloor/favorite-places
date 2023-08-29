802875ba80ac9fa4591ff3fffaf0e25e0bff2980
Emil Noren <stratosfloor@hotmail.com>
2023-08-29 07:37:59 +0200

    Set up basic flutter project

---
+++ lib/screens/favorite_places.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FavoritePlaces extends StatelessWidget {
+  const FavoritePlaces({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('hej'),
+    );
+  }
+}
