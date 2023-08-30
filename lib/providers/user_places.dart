e1d708d0832d859bfb2255ee441351e35ca8daaf
Emil Noren <stratosfloor@hotmail.com>
2023-08-30 16:52:49 +0200

    Implemented storing of places with sqflite

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -2,20 +2,74 @@ import 'dart:io';
 
 import 'package:favorite_places/models/place.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:path_provider/path_provider.dart' as syspath;
+import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
+
+Future<Database> _getDatabase() async {
+  final dbPath = await sql.getDatabasesPath();
+  final db = await sql.openDatabase(
+    path.join(dbPath, 'places.db'),
+    onCreate: (db, version) {
+      return db.execute(
+          'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL, adress TEXT)');
+    },
+    version: 1,
+  );
+  return db;
+}
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
+
+  Future<void> loadPlaces() async {
+    final db = await _getDatabase();
+    final data = await db.query('user_places');
+
+    final places = data
+        .map(
+          (place) => Place(
+            id: place['id'] as String,
+            title: place['title'] as String,
+            image: File(place['image'] as String),
+            location: PlaceLocation(
+              latitude: place['lat'] as double,
+              longitude: place['lng'] as double,
+              adress: place['adress'] as String,
+            ),
+          ),
+        )
+        .toList();
+    state = places;
+  }
 
   void addPlace(
     String title,
     File image,
     PlaceLocation location,
-  ) {
+  ) async {
+    final appDir = await syspath.getApplicationDocumentsDirectory();
+    final filename = path.basename(image.path);
+    final copiedImage = await image.copy('${appDir.path}/$filename');
+
     final newPlace = Place(
       title: title,
-      image: image,
+      image: copiedImage,
       location: location,
     );
+
+    final db = await _getDatabase();
+
+    db.insert('user_places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+      'lat': newPlace.location.latitude,
+      'lng': newPlace.location.longitude,
+      'adress': newPlace.location.adress,
+    });
+
     state = [...state, newPlace];
   }
 }
